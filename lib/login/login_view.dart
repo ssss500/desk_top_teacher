4d6921c1d5c3326c2ab67f9512e61b17f93bce91
saif ahmed <50845889+ssss500@users.noreply.github.com>
2023-12-23 22:53:30 +0200

    windos

--- lib/login/login_view.dart
+++ lib/login/login_view.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -7,11 +9,8 @@ import '../widgte/custom_text.dart';
 import 'login_controller.dart';
 
 class LoginView extends StatelessWidget {
-
-
   LoginView({
     super.key,
-
   });
 
   final LoginController controller = Get.put(LoginController());
@@ -38,122 +37,139 @@ class LoginView extends StatelessWidget {
               ),
             ],
           ),
-          child: Row(
-            children: [
-              Expanded(
-                child: Container(
-                  height: height * 0.8,
-                  decoration: BoxDecoration(
-                    color: Colors.grey.withOpacity(0.5),
-                    borderRadius: const BorderRadius.only(
-                      topLeft: Radius.circular(20),
-                      bottomLeft: Radius.circular(20),
-                    ),
-                    //image background
-                    image:  const DecorationImage(
-                      image: AssetImage("assets/images/bg.jpg"),
-                      fit: BoxFit.cover,
-                    ),
+          child: isWindows8or7()
+              ? Center(
+                  //
+                  //لا تعمل علي الويندوز
+                  child: CustomText(
+                    "البرنامج لا يعمل علي اصدار الويندوز الخاص بك يرجي التحديث ل 10 او 11",
+                    color: Colors.black,
                   ),
-                  child: SingleChildScrollView(
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                      Image.asset(
-                      "assets/images/logo.png",
-                      height: 300,
-                    ),
-                        const SizedBox(
-                          height: 20,
-                        ),
-                        //registration instructions using CustomText and space
-                        CustomText(
-                          "تعليمات التسجيل",
-                          color: Colors.white,
-                        ),
-                        space(),
-                        CustomText(
-                          "1- ادخل علي الفيديو الذي تريد مشاهدته",
-                          color: Colors.white,
-                        ),
-                        space(),
-                        CustomText(
-                          "2- اضغط علي ⋮ اعلي الفيديو علي اليمين",
-                          color: Colors.white,
-                        ),
-                        space(),
-                        CustomText(
-                          "3- اضغط علي مشاهدة الفيديو علي الكمبيوتر",
-                          color: Colors.white,
-                        ),
-                        space(),
-                        CustomText(
-                          "4- عند فتح الكاميره قم بعمل سكان للكود",
-                          color: Colors.white,
-                        ),
-                        space(),
-                        space(),
-                        space(),
-                        CustomText(
-                          "${controller.myVersion} V",
-                          color: Colors.white,
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              ),
-              Expanded(
-                child:
-                    //qr code
-                    Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
+                )
+              : Row(
                   children: [
-                    GetBuilder<LoginController>(builder: (controller) {
-                      return Column(
-                        children: [
-                          if (controller.qrModel.id == "")
-                            const CircularProgressIndicator()
-                          else
-                            QrImageView(
-                              data: controller.qrModel.id.toString(),
-                              version: QrVersions.auto,
-                              size: 300.0,
-                            ),
-                          // SizedBox(
-                          //   height: 300,
-                          //   width: 300,
-                          //   child: PrettyQrView.data(
-                          //     data: controller.qrModel.id.toString(),
-                          //
-                          //     decoration: const PrettyQrDecoration(
-                          //       image: PrettyQrDecorationImage(
-                          //         image: AssetImage('assets/images/logo.png'),
-                          //       ),
-                          //     ),
-                          //     // version: QrVersions.auto,
-                          //     // size: 300.0,
-                          //   ),
-                          // ),
-                          SizedBox(
-                            height: 20,
+                    Expanded(
+                      child: Container(
+                        height: height * 0.8,
+                        decoration: BoxDecoration(
+                          color: Colors.grey.withOpacity(0.5),
+                          borderRadius: const BorderRadius.only(
+                            topLeft: Radius.circular(20),
+                            bottomLeft: Radius.circular(20),
                           ),
-                          if (kDebugMode)
-                            CustomText(
-                              controller.qrModel.id.toString(),
-                              color: Colors.black,
-                            ),
+                          //image background
+                          image: const DecorationImage(
+                            image: AssetImage("assets/images/bg.jpg"),
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                        child: SingleChildScrollView(
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Image.asset(
+                                "assets/images/logo.png",
+                                height: 300,
+                              ),
+                              const SizedBox(
+                                height: 20,
+                              ),
+                              //registration instructions using CustomText and space
+                              CustomText(
+                                "تعليمات التسجيل",
+                                color: Colors.white,
+                              ),
+                              space(),
+                              CustomText(
+                                "1- ادخل علي الفيديو الذي تريد مشاهدته",
+                                color: Colors.white,
+                              ),
+                              space(),
+                              CustomText(
+                                "2- اضغط علي ⋮ اعلي الفيديو علي اليمين",
+                                color: Colors.white,
+                              ),
+                              space(),
+                              CustomText(
+                                "3- اضغط علي مشاهدة الفيديو علي الكمبيوتر",
+                                color: Colors.white,
+                              ),
+                              space(),
+                              CustomText(
+                                "4- عند فتح الكاميره قبل بعمل سكان للكود",
+                                color: Colors.white,
+                              ),
+                              space(),
+                              space(),
+                              space(),
+                              CustomText(
+                                "${controller.myVersion} V",
+                                color: Colors.white,
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child:
+                          //qr code
+                          Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          GetBuilder<LoginController>(builder: (controller) {
+                            return Column(
+                              children: [
+                                if (controller.qrModel.id == "")
+                                  const CircularProgressIndicator()
+                                else
+                                  QrImageView(
+                                    data: controller.qrModel.id.toString(),
+                                    version: QrVersions.auto,
+                                    size: 300.0,
+                                  ),
+                                // SizedBox(
+                                //   height: 300,
+                                //   width: 300,
+                                //   child: PrettyQrView.data(
+                                //     data: controller.qrModel.id.toString(),
+                                //
+                                //     decoration: const PrettyQrDecoration(
+                                //       image: PrettyQrDecorationImage(
+                                //         image: AssetImage('assets/images/logo.png'),
+                                //       ),
+                                //     ),
+                                //     // version: QrVersions.auto,
+                                //     // size: 300.0,
+                                //   ),
+                                // ),
+                                SizedBox(
+                                  height: 20,
+                                ),
+                                if (kDebugMode)
+                                  CustomText(
+                                    controller.qrModel.id.toString(),
+                                    color: Colors.black,
+                                  ),
+                              ],
+                            );
+                          })
                         ],
-                      );
-                    })
+                      ),
+                    ),
                   ],
                 ),
-              ),
-            ],
-          ),
         ),
       ),
     );
+  }
+
+  bool isWindows8or7() {
+    if (Platform.isWindows) {
+      final version = Platform.operatingSystemVersion;
+      return version.contains("Windows 8") || version.contains("Windows 7");
+    }
+    return false;
   }
 
   space() {
